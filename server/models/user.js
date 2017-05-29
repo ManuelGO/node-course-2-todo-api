4727a32a42f22c6f45e30fa7e0ca4577e97ae0ef
Manuel Gonzalez <manuelarvelo19@gmail.com>
2017-05-28 21:11:37 -0400

    new commit with mongoose and more.

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User',{
+	email: {
+		required: true,
+		minlength: 1,
+		type: String,
+		trim: true
+	}
+});
+
+module.exports = {User};
