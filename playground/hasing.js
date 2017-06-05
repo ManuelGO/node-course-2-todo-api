1d2aaee9fce020de79147b4da0c5e5ca14c7d1e5
Manuel Gonzalez <manuelarvelo19@gmail.com>
2017-06-04 21:30:23 -0400

    Hash user password before saving

--- playground/hasing.js
+++ playground/hasing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-var decoded = jwt.verify(token, '123abc');
-console.log(token);
-console.log('-------');
-console.log(decoded);
+// bcrypt.genSalt(10, (err, salt)=>{
+// 	bcrypt.hash(password, salt, (err, hash)=>{
+// 		console.log(hash);
+// 	});
+// });
+
+var hp = '$2a$10$iwClWvjfM8s7Ybw9EQK5dO6Cx5ftrwMMtRO9l6CMT/YfzQ2LQIEOy';
+bcrypt.compare(password, hp, (err, res)=>{
+	console.log(res);
+})
+
+
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// var decoded = jwt.verify(token, '123abc');
+// console.log(token);
+// console.log('-------');
+// console.log(decoded);
 
 //jwt hace que lo comentado no sea necesario:
 
